ba6ed40a694e5f6b36f9a734a15f1f0d791c8297
ZeeDabs <dabbagh.zainab@gmail.com>
2023-03-14 23:08:21 -0400

    final-ish code

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,7 @@ $( function() {
 
 apiSearchButton.addEventListener("click", function () {
   apiRequest()
+  localStorage.setItem('selectedName', $('#selectedName').val())
 })
 
 // yyyymmdd userInput.dayjs().format('YYYYMMDD)
@@ -39,9 +40,9 @@ function apiRequest() {
   localStorage.setItem('selectedName', selectedName)
 
 
-  var apiKey = 'tjyebbtQOUAnsp7tZpC8fCtH2pW8s3a6'
+  var nytKey = 'tjyebbtQOUAnsp7tZpC8fCtH2pW8s3a6'
 
-  var nytUrl =  `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${nytDate}&end_date=${nytDate}&api-key=`+ apiKey;
+  var nytUrl =  `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${nytDate}&end_date=${nytDate}&api-key=`+ nytKey;
 
   fetch(nytUrl)
   .then(response => response.json()) 
@@ -52,27 +53,11 @@ function apiRequest() {
       console.log(article.headline.main)
       
       var nytInfoId = "#nytInfo" + index
-      var nytHeadline = document.querySelector(nytInfoId + " h4")
-      var abstracts = document.querySelector(nytInfoId + " span")
+      var nytHeadline = document.querySelector(nytInfoId + " h5")
+      var abstracts = document.querySelector(nytInfoId + " p")
       
       nytHeadline.innerHTML = article.headline.main
       abstracts.innerHTML = article.abstract
-
-      // nytHeadline.innerText = article.headline.main
-      // abstracts.innerText = article.abstract
-
-      // for (var i = 0; i < nytApiHeads.length; i++) {
-      //   nytHeadline.innerText = article.headline.main
-      // }
-
-      
-      
-
-
-      // var h1 = document.createElement('h1')
-      // h1.innerText = article.headline.main
-
-      // headlineDiv.appendChild(h1);
 
     })
 
@@ -91,12 +76,26 @@ function apiRequest() {
 .then (data => {
   console.log(data);
 
-  data.selected.slice(0, 3).map(selectedEvents => {
+  data.selected.slice(0, 3).map((selectedEvents, index) => {
       console.log(selectedEvents.text)
 
-      // var slide1Abstract = document.getElementById('slide-1-abstract')
-      // slide1Abstract.innerText = selectedEvents.text
-    
+      var selectedEventsId = "#happenedToday" + index
+      var eventsImgID = "#todayImg" + index
+      var eventName = document.querySelector(selectedEventsId + " h5")
+      var eventDesc = document.querySelector(selectedEventsId + " p")
+      var eventImg = document.querySelector(eventsImgID + " img")
+
+      eventName.innerHTML = selectedEvents.pages[0].normalizedtitle
+      eventDesc.innerHTML = selectedEvents.pages[0].extract
+
+      if ('thumbnail' in selectedEvents.pages[0]) {
+        eventImg.setAttribute('src', selectedEvents.pages[0].thumbnail.source);
+        eventImg.setAttribute('alt', selectedEvents.pages[0].description)
+        eventImg.setAttribute('width', '100px')
+      } else {
+        eventImg.setAttribute('src', './assets/images/partyhorn.png')
+      }
+      
     
   })
 });
@@ -112,10 +111,27 @@ fetch( wikiBirthsUrl,
 ).then(response => response.json()) 
 .then (data => {
 console.log(data);
-data.births.slice(0, 3).map(selectedBirths => {
+data.births.slice(0, 3).map((selectedBirths, index) => {
     console.log(selectedBirths.pages[0].normalizedtitle);
+    console.log(selectedBirths.pages[0].extract)
     
-  
+    var otherBirthsID = "#otherBirths" + index
+    var birthsImgID = "#birthsImg" + index
+    var wikiBirthsName = document.querySelector(otherBirthsID + " h5")
+    var wikiBirthsAbstract = document.querySelector(otherBirthsID + " p")
+    var wikiImage = document.querySelector(birthsImgID + " img")
+
+    wikiBirthsName.innerHTML = selectedBirths.pages[0].normalizedtitle
+    wikiBirthsAbstract.innerHTML = selectedBirths.pages[0].extract
+
+    if ('thumbnail' in selectedBirths.pages[0]) {
+      wikiImage.setAttribute('src', selectedBirths.pages[0].thumbnail.source);
+      wikiImage.setAttribute('alt', selectedBirths.pages[0].description)
+      wikiImage.setAttribute('width', '100px')
+    } else {
+      wikiImage.setAttribute('src', './assets/images/partyhorn.png')
+    }
+   
   
 })
 });
@@ -123,9 +139,64 @@ data.births.slice(0, 3).map(selectedBirths => {
 
 });
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2b7cad8979mshfa5e2dad08c6573p12d605jsn5240ab6f6aac',
+		'X-RapidAPI-Host': 'billboard-api2.p.rapidapi.com'
+	}
+};
+
+fetch(`https://billboard-api2.p.rapidapi.com/hot-100?date=${billboardDate}&range=1-10`, options)
+	.then(response => response.json())
+	.then(data => {
+
+    console.log(data);
+    console.log(data.content[1].artist)  
+
+
+    var firstArtist= data.content[1].artist
+    var secondArtist = data.content[2].artist
+    var thirdArtist = data.content[3].artist
+
+    var firstSong = data.content[1].title
+    var secondSong = data.content[2].title
+    var thirdSong = data.content[3].title
+
+    var firstImg = data.content[1].image
+    var secondImg = data.content[2].image
+    var thirdImg = data.content[3].image
+
+    document.getElementById('firstSong').innerHTML=firstSong
+    document.getElementById('secondSong').innerHTML=secondSong
+    document.getElementById('thirdSong').innerHTML=thirdSong
+
+    document.getElementById('firstArtist').innerHTML=firstArtist
+    document.getElementById('secondArtist').innerHTML=secondArtist
+    document.getElementById('thirdArtist').innerHTML=thirdArtist
+
+    document.getElementById('firstImg').setAttribute('src', firstImg)
+    document.getElementById('firstImg').setAttribute('width', '200px')
+    document.getElementById('secondImg').setAttribute('src', secondImg)
+    document.getElementById('secondImg').setAttribute('width', '200px')
+    document.getElementById('thirdImg').setAttribute('src', thirdImg)
+    document.getElementById('thirdImg').setAttribute('width', '200px')
+  })
+    
+
+
+	.catch(err => console.error(err));
+    
+
+  
+
+
+}
+
 var bdayBtn = document.getElementById('bdayBtn');
 
 bdayBtn.addEventListener('click', function() {
+  console.log(localStorage.getItem('selectedName'))
   document.getElementById('frontPage').classList.add('hide');
   document.getElementById('infoPage').classList.add('hide')
   document.getElementById('happyBirthday').classList.remove('hide');
@@ -146,188 +217,4 @@ bdayBtn.addEventListener('click', function() {
   stop();
 })
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '2b7cad8979mshfa5e2dad08c6573p12d605jsn5240ab6f6aac',
-// 		'X-RapidAPI-Host': 'billboard-api2.p.rapidapi.com'
-// 	}
-// };
-
-// fetch(`https://billboard-api2.p.rapidapi.com/hot-100?date=${billboardDate}&range=1-10`, options)
-// 	.then(response => response.json())
-// 	.then(data => {
-
-//     JSON.stringify(data)
-//     console.log(data);
-//   //   data.content.1(topSongs => {
-//   //     console.log(topSongs.title)
-    
-    
-//   // });
-
-//   })
-// 	// .catch(err => console.error(err));
-    
-
-  
-}
-
-
-
-
-// // let url = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${wikiMonth}/${wikiDay}`;
-
-// // fetch( url,
-// //     {
-// //         headers: {
-// //             'Authorization': 'd2ded8271ec6706b0494e89771c0a5c3',
-// //             'Api-User-Agent': 'ITS_YOUR_PARTY (dabbagh.zainab@gmail.com)'
-// //         }
-// //     }
-// // ).then(response => response.json()) 
-// // .then (data => {
-// //   console.log(data);
-// // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // _______________________________________________________
-// // API CODE
-// // _______________________________________________________
-
-// // wikipedia client ID: d2ded8271ec6706b0494e89771c0a5c3
-
-// // wikipedia secret: dd343f361c757fd8a9eabc2c49803e270d1cc042
-
-
-// // 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=[YOUR_API_KEY]'
-// // tjyebbtQOUAnsp7tZpC8fCtH2pW8s3a6
-
-
-// //   'https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=19900301&end_date=19900301&api-key=tjyebbtQOUAnsp7tZpC8fCtH2pW8s3a6' \ //article search 
-
-// // 'https://api.nytimes.com/svc/books/v3/lists.json?list=fiction&bestsellers-date=1990-03-01&api-key=tjyebbtQOUAnsp7tZpC8fCtH2pW8s3a6' //best seller search
-
-// // http://api.marketstack.com/v1/eod?access_key=18e6649598844cabcb3fed79128d93b0&symbols=AAPL&date_from=1990-02-27&date_to=1990-02-28
-
-
-// // var apiKey = 'tjyebbtQOUAnsp7tZpC8fCtH2pW8s3a6'
-// // var headlineDiv = document.getElementById('headlines')
-
-// // searchBtn.addEventListener('click', function(){
-// //   var dateInput = $('#userInput').val();
-// //   var requestUrl =  `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${dateInput}&end_date=${dateInput}&api-key=`+ apiKey;
-
-// //   fetch(requestUrl)
-// //   .then(response => response.json()) 
-// //   .then (data => {
-// //     console.log(data);
-
-// //     data.response.docs.map(article => {
-// //       console.log(article.headline.main)
-// //       // var h1 = document.createElement('h1')
-// //       // h1.innerText = article.headline.main
-
-// //       // headlineDiv.appendChild(h1);
-
-// //     })
-
-
-// //   })
-
-// // })
-
-// // let today = new Date();
-// // let month = String(today.getMonth() + 1).padStart(2,'0');
-// // let day = String(today.getDate()).padStart(2,'0');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // // var requestUrl = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/births/0301`
-
-// // // // ${userInput}};
-
-// // // fetch(requestUrl)
-// // // .then(response => response.json())
-// // // .then (data => {
-// // //   console.log(data)
-
-// // //   // data.births.map (pages => { //cite shortcut (find youtube video where i learned this)
-
-// // //   //   var p = document.createElement('p')
-// // //   //   p.innerText = births.description;
-
-// // //   //   var h1 = document.createElement('h1')
-// // //   //   h1.innerText = births[0].pages[0].title;
-
-    
-// // //   //   headlines.appendChild(img)
-// // //   //   headlines.appendChild(p);
-    
-// // //   })
-// // // // })
 
